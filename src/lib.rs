d78271bd127e73c6482816f1e224c78679505403
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-01-16 17:40:08 +0100

    chore: move serde_string to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 #[macro_use]
 mod macros;
 
+mod serde_string;
+
 mod response;
 use response::Response;
 pub use response::{ClientError, ResponseError, ResponseErrorMessage, Result};
@@ -76,89 +78,4 @@ fn concat_1(base_path: &str, value: &str) -> String {
     s.push_str(base_path);
     s.push_str(value);
     s
-}
-
-mod serde_string {
-    use serde::{Deserialize, Deserializer, Serializer};
-    use std::{fmt::Display, str::FromStr};
-
-    pub fn serialize<T: ToString, S: Serializer>(value: T, s: S) -> Result<S::Ok, S::Error> {
-        s.serialize_str(&value.to_string())
-    }
-
-    pub fn deserialize<'de, E, T, D>(d: D) -> Result<T, D::Error>
-    where
-        T: FromStr<Err = E>,
-        E: Display,
-        D: Deserializer<'de>,
-    {
-        String::deserialize(d).and_then(|s| T::from_str(&s).map_err(serde::de::Error::custom))
-    }
-
-    pub mod option {
-        use super::*;
-
-        pub fn serialize<T: ToString, S: Serializer>(
-            value: &Option<T>,
-            s: S,
-        ) -> Result<S::Ok, S::Error> {
-            if let Some(value) = value {
-                s.serialize_some(&value.to_string())
-            } else {
-                s.serialize_none()
-            }
-        }
-
-        pub fn deserialize<'de, E, T, D>(d: D) -> Result<Option<T>, D::Error>
-        where
-            T: FromStr<Err = E>,
-            E: Display,
-            D: Deserializer<'de>,
-        {
-            match <Option<String>>::deserialize(d) {
-                Ok(Some(s)) => {
-                    if s.is_empty() {
-                        Ok(None)
-                    } else {
-                        match T::from_str(&s) {
-                            Ok(x) => Ok(Some(x)),
-                            Err(e) => Err(serde::de::Error::custom(e)),
-                        }
-                    }
-                }
-                Ok(None) => Ok(None),
-                Err(e) => Err(e),
-            }
-        }
-    }
-
-    pub mod vec {
-        use super::*;
-        use serde::ser::SerializeSeq;
-
-        pub fn serialize<T: ToString, S: Serializer>(
-            value: &Vec<T>,
-            s: S,
-        ) -> Result<S::Ok, S::Error> {
-            let mut seq = s.serialize_seq(Some(value.len()))?;
-            for value in value {
-                seq.serialize_element(&value.to_string())?;
-            }
-            seq.end()
-        }
-
-        pub fn deserialize<'de, E, T, D>(d: D) -> Result<Vec<T>, D::Error>
-        where
-            T: FromStr<Err = E>,
-            E: Display,
-            D: Deserializer<'de>,
-        {
-            <Vec<String>>::deserialize(d).and_then(|v| {
-                v.into_iter()
-                    .map(|s| T::from_str(&s))
-                    .collect::<Result<Vec<T>, _>>()
-                    .map_err(serde::de::Error::custom)
-            })
-        }
-    }
 }
